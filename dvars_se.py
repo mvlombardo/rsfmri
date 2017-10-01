9a6edf5b787e419c2c17c154bcb066c0fd2533da
mvlombardo <mvlombardo@gmail.com>
2017-10-01 08:14:13 +0300

    initial commit

---
+++ dvars_se.py
@@ -0,0 +1,66 @@
+# dvars_se.py
+"""
+Compute DVARS on single echo EPI data.
+
+For usage see:  python dvars_se.py -h
+
+Example usage:
+python dvars_se.py -d rest_pp.nii.gz
+"""
+
+# import modules
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+from optparse import OptionParser
+
+
+# function to parse input arguments
+def parse_args():
+    """
+    Parse arguments.
+    """
+    parser=OptionParser()
+    parser.add_option('-d',"",dest='data',help="rest_pp.nii.gz file ex: -d rest_pp.nii.gz",default=None)
+    parser.add_option('-p',"",action="store_true",dest='plot',help="make dvars plot",default=False)
+    (options,args) = parser.parse_args()
+    return(options)
+
+
+# boilerplate code to call main code for executing
+if __name__ == '__main__':
+
+    # Parse arguments
+    opts = parse_args()
+    data_file = opts.data
+
+    # load data
+    nii = data
+    fn=nib.load(nii)
+
+    # get data and vectorize
+    nx,ny,nz,nt = fn.get_data().shape
+    data = fn.get_data().reshape([nx*ny*nz,nt]).T
+
+    # compute mean
+    d_mu = data.mean(0)
+
+    # compute mask
+    d_mask = d_mu!=0
+
+    # grab voxels within mask
+    db=data[:,d_mask]
+
+    # compute DVARS
+    dbdt = np.abs(np.diff(db,n=1,axis=0))+0.0000001
+    dvars = np.sqrt(np.mean(dbdt**2,1))
+
+    # save DVARS to text file
+    np.savetxt('%s_dvars.txt' % nii.split('.')[0],dvars)
+
+    # make plot
+    if opts.plot:
+        plt.plot(dvars)
+        plt.xlabel("Frame #")
+        plt.ylabel("DVARS (%x10)")
+        plt.show()
