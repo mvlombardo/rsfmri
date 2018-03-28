3dfaab71a6096de07dbf5c40b7eae4fcb7ddf4f6
mvlombardo <mvlombardo@gmail.com>
2018-03-28 13:12:27 +0300

    added nreg argument

--- parcellate.m
+++ parcellate.m
@@ -1,4 +1,4 @@
-function result = parcellate(atlasfile, datafile, fname2save, MEANCENTER)
+function result = parcellate(atlasfile, datafile, fname2save, MEANCENTER, nreg)
 %   parcellate - Extract mean time series from parcellation
 %
 %   Depends on read_avw.m function from FSL to read in *nii.gz files
@@ -8,12 +8,13 @@ function result = parcellate(atlasfile, datafile, fname2save, MEANCENTER)
 %       datafile = data to use
 %       fname2save = prefix to use for saving file
 %       mean_center = set to 1 to remove mean, otherwise set to 0
+%       nreg = number of regions
 %
 %   OUTPUT
-%       result = table [nvols, nparcels]        
+%       result = table [nvols, nparcels]
 %
 %   Example usage:
-%   
+%
 %   atlasfile = 'MMP_in_MNI_symmetrical_1_resamp.nii.gz';
 %   datafile = 'P01_1_T1c_medn_nlw.nii.gz';
 %   fname2save = 'parc_hcpsymm_medn.csv';
@@ -25,7 +26,8 @@ function result = parcellate(atlasfile, datafile, fname2save, MEANCENTER)
 %% read in parcellation
 [atlas, dims,scales,bpp,endian] = read_avw(atlasfile);
 % find unique parcel numbers, and remove parcel 0
-parc_num = unique(atlas); parc_num(parc_num==0) = [];
+% parc_num = unique(atlas); parc_num(parc_num==0) = [];
+parc_num = 1:nreg;
 
 %% read in data
 [data, dims,scales,bpp,endian] = read_avw(datafile);
@@ -34,15 +36,18 @@ parc_num = unique(atlas); parc_num(parc_num==0) = [];
 for i = 1:length(parc_num)
     % make binary ROI mask for specific parcel
     mask = ismember(atlas,parc_num(i));
-    
-    % loop over timepoints
-    for ivol = 1:size(data,4)
-        % grab specific timepoint
-        tmp_vol = data(:,:,:,ivol);
-        % compute mean within parcel mask
-        result(ivol,i) = nanmean(tmp_vol(mask));
-    end % for i
 
+    if sum(mask(:))==0
+        result(:,i) = NaN;
+    else
+        % loop over timepoints
+        for ivol = 1:size(data,4)
+            % grab specific timepoint
+            tmp_vol = data(:,:,:,ivol);
+            % compute mean within parcel mask
+            result(ivol,i) = nanmean(tmp_vol(mask));
+        end % for i
+    end % if
 end % for ivol
 
 
